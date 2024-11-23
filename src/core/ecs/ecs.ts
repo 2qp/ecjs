eeca3aa52ec74a90685eef6269399d219f700920
2qp <56475797+2qp@users.noreply.github.com>
2024-11-24 01:21:42 +0530

    chore(ecs): opt in to type-only imports

--- src/core/ecs/ecs.ts
+++ src/core/ecs/ecs.ts
@@ -1,12 +1,11 @@
-import { Entities } from "../entity/entities";
-import { Pools } from "../pools/pools";
-import { System } from "../system/system";
+import type { Entities } from "../entity/entities";
+import type { Pools } from "../pools/pools";
+import type { System } from "../system/system";
 
 type ECS<T> = {
   entities: Entities;
   pools: Pools<T>;
   systems: System<T>[];
-
   initialized: boolean | null;
 };
 
