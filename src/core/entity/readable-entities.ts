004b45a776472b871e21cd28e2214490c02e1f68
2qp <56475797+2qp@users.noreply.github.com>
2024-11-24 01:34:58 +0530

    chore(entity): add generic ReadableEntity/ies type definition

---
+++ src/core/entity/readable-entities.ts
@@ -0,0 +1,5 @@
+import { ReadableEntity } from "./readable-entity";
+
+type ReadableEntities = Map<number, ReadableEntity>;
+
+export type { ReadableEntities };
---
+++ src/core/entity/readable-entity.ts
@@ -0,0 +1,6 @@
+type ReadableEntity = {
+  id: number;
+  pools: string[];
+};
+
+export type { ReadableEntity };
