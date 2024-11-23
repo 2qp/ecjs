8cfbe68195ed0270c5c8c97cc6fe0b9e16a8df7b
2qp <56475797+2qp@users.noreply.github.com>
2024-11-24 01:29:44 +0530

    docs(entity): add JSDoc comment

--- src/core/entity/entity.ts
+++ src/core/entity/entity.ts
@@ -1,3 +1,8 @@
+/**
+ * Use a utility like `readEntity` to convert the `pools` Set to an array.
+ *
+ * making the `Entity` more readable (e.g., for JSON or API).
+ */
 type Entity = {
   id: number;
   pools: Set<string>;
