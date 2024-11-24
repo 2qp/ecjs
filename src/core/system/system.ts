9025ffdb8075f31f57ca7c896ef2c61215ef2a99
2qp <56475797+2qp@users.noreply.github.com>
2024-11-24 21:30:31 +0530

    chore(system): add union callback to System type definition

--- src/core/system/system.ts
+++ src/core/system/system.ts
@@ -1,11 +1,11 @@
-import { Pools } from "../pools/pools";
+import type { Pools } from "../pools/pools";
 
 type System<T> = {
-  update: (pools: Pools<T>, delta: number) => void;
+  update: (pools: Pools<T>, delta: number) => void | Promise<void>;
   priority?: number;
-  initialize?: () => void;
+  initialize?: () => void | Promise<void>;
   initialized?: boolean;
-  cleanup?: () => void;
+  cleanup?: () => void | Promise<void>;
 };
 
 export type { System };
