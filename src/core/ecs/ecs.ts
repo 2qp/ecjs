0dd094051dbf273ef48258493b7a202fd35e0c84
2qp <56475797+2qp@users.noreply.github.com>
2024-11-26 02:14:47 +0530

    chore(ecs): add generic ECS type based on ECSBase

--- src/core/ecs/ecs.ts
+++ src/core/ecs/ecs.ts
@@ -1,12 +1,19 @@
 import type { Entities } from "../entity/entities";
+import type { EntityBase } from "../entity/entity";
 import type { Pools } from "../pools/pools";
-import type { System } from "../system/system";
+import type { System, SystemBase } from "../system/system";
 
-type ECS<T> = {
-  entities: Entities;
+type ECSBase<T extends SystemBase<T>, U extends EntityBase> = {
+  entities: Entities<U>;
   pools: Pools<T>;
   systems: System<T>[];
   initialized: boolean | null;
 };
+
+type ECS<
+  T extends ECSBase<U, V>,
+  U extends SystemBase<U>,
+  V extends EntityBase
+> = T;
 
 export type { ECS };
