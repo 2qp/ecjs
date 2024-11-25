a21cbdeb80eb5745eb1314b3732d0a2a90916f19
2qp <56475797+2qp@users.noreply.github.com>
2024-11-26 03:36:19 +0530

    refactor(addEntity): remove unnecessary eid prop

--- src/core/entity/addEntity.ts
+++ src/core/entity/addEntity.ts
@@ -4,13 +4,12 @@ import type { Entity, EntityBase } from "./entity";
 // type AddEntityParams = {};
 
 type AddEntityType = <T extends EntityBase>(
-  eid: number,
   entity: Entity<T>,
   entities: Entities<T>
 ) => Entities<T>;
 
-const addEntity: AddEntityType = (eid, entity, entities) => {
-  const updatedEntities = new Map(entities).set(eid, entity);
+const addEntity: AddEntityType = (entity, entities) => {
+  const updatedEntities = new Map(entities).set(entity.id, entity);
 
   return updatedEntities;
 };
