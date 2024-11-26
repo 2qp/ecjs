4cc41e2eae84d53822f9466496d2000e506ee62a
2qp <56475797+2qp@users.noreply.github.com>
2024-11-27 01:22:04 +0530

    feat(system): add entry point

---
+++ src/core/system/index.ts
@@ -0,0 +1,6 @@
+export { addSystem } from "./addSystem";
+export { runSystems } from "./runSystems";
+
+export type { AddSystemType } from "./addSystem";
+export type { RunSystemsType } from "./runSystems";
+export type { System, SystemBase } from "./system";
