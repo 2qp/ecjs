412f65b0c8c0d1bfc2ef5ef85bc2acd0d17bc822
2qp <56475797+2qp@users.noreply.github.com>
2024-11-27 01:01:11 +0530

    chore(config): add multiple entry points to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,37 @@
 /// <reference types="vite/client" />
 
 import react from "@vitejs/plugin-react-swc";
-import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ tsconfigPath: "./tsconfig.app.json" })],
+  plugins: [
+    react(),
+    dts({
+      tsconfigPath: "./tsconfig.app.json",
+      insertTypesEntry: true,
+      outDir: "dist",
+    }),
+  ],
 
   build: {
+    emptyOutDir: false,
+    outDir: "dist",
+    sourcemap: true,
+
     lib: {
-      entry: resolve(__dirname, "src/main.ts"),
-      formats: ["es"],
+      entry: {
+        component: "./src/core/component/index.ts",
+        entity: "./src/core/entity/index.ts",
+      },
+      formats: ["es", "cjs"],
+      name: "ecjs",
     },
 
     rollupOptions: {
       external: ["react", "react-dom", "react/jsx-runtime"],
+
       output: {
         globals: {
           react: "React",
