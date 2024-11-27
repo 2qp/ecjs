44b48935809f73399d161691363c0cbaf8e8c8f8
2qp <56475797+2qp@users.noreply.github.com>
2024-11-28 01:06:37 +0530

    chore(entity): update type exports on subPath entry point

--- src/core/entity/index.ts
+++ src/core/entity/index.ts
@@ -1,2 +1,14 @@
+export { addEntity } from "./addEntity";
 export { createEntity } from "./createEntity";
 export { getEntity } from "./getEntity";
+export { readEntity } from "./readEntity";
+export { removeEntity } from "./removeEntity";
+export { updateEntity } from "./updateEntity";
+
+export type { AddEntityType } from "./addEntity";
+export type { CreateEntityType } from "./createEntity";
+export type { GetEntityType } from "./getEntity";
+export type { ReadableEntity } from "./readable-entity";
+export type { ReadEntityType } from "./readEntity";
+export type { RemoveEntityType } from "./removeEntity";
+export type { UpdateEntityType } from "./updateEntity";
