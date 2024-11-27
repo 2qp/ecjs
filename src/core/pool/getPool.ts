3b3180e2245c8641ad5783c9f2f15acf79955eff
2qp <56475797+2qp@users.noreply.github.com>
2024-11-28 00:58:57 +0530

    chore(pool): fix typo in getPool type definition

--- src/core/pool/getPool.ts
+++ src/core/pool/getPool.ts
@@ -3,9 +3,9 @@ import type { Pool } from "./pool";
 
 // type getPoolParams = {};
 
-type getPoolType = <T>(pool: string, pools: Pools<T>) => Pool<T> | undefined;
+type GetPoolType = <T>(pool: string, pools: Pools<T>) => Pool<T> | undefined;
 
-const getPool: getPoolType = (pool, pools) => pools.get(pool);
+const getPool: GetPoolType = (pool, pools) => pools.get(pool);
 
 export { getPool };
-export type { getPoolType };
+export type { GetPoolType };
