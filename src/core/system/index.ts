ce9fff69300dafaf4ab51f046947631de8349f64
2qp <56475797+2qp@users.noreply.github.com>
2024-12-02 00:58:35 +0530

    feat(system): remove async system runners
    - will run back on event driven system runners

--- src/core/system/index.ts
+++ src/core/system/index.ts
@@ -2,13 +2,9 @@ export { addSystem } from "./addSystem";
 export { addSystems } from "./addSystems";
 export { createSystem } from "./createSystem";
 export { runSystems } from "./runSystems";
-export { runSystemsConcurrently } from "./runSystemsConcurrently";
-export { runSystemsWithConcurrencyControl } from "./runSystemsWithConcurrencyControl";
 
 export type { AddSystemType } from "./addSystem";
 export type { AddSystemsType } from "./addSystems";
 export type { CreateSystemParams, CreateSystemType } from "./createSystem";
 export type { RunSystemsType } from "./runSystems";
-export type { RunSystemsConcurrentlyType } from "./runSystemsConcurrently";
-export type { RunSystemsWithConcurrencyControlType } from "./runSystemsWithConcurrencyControl";
 export type { System, SystemBase } from "./system";
